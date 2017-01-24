be1900b6b9404b3583bc2af6513078f352059ffb
nassy <stasya.aska@gmail.com>
2017-01-24 11:24:40 -0500

    connected models (sqlite3) for hobies (index.html), works (work.html), and schools

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Work
+from .models import Education
+from .models import Hobbies
 
-# Register your models here.
+admin.site.register(Work)
+admin.site.register(Education)
+admin.site.register(Hobbies)
--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Work(models.Model):
+    organization = models.CharField(max_length=32)
+    position = models.CharField(max_length=32)
+    period = models.CharField(max_length=32)
+    #to do: split period for date_of_start and finish work
+    responsibilities = models.TextField()
+    def __str__(self):
+        return self.organization
+
+class Hobbies(models.Model):
+    name = models.CharField(max_length=32)
+    def __str__(self):
+        return self.name
+
+class Education(models.Model):
+    period = models.CharField(max_length=32)
+    #to do: split to start & end date
+    name = models.CharField(max_length=32)
+    specialization = models.CharField(max_length=32)
+    url = models.CharField(max_length=64)
+    def __str__(self):
+        return self.name
